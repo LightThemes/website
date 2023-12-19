52ff7620c6faa65e9acce6233ddd0af8ecd68258
LightThemes <84159573+LightThemes@users.noreply.github.com>
2023-12-19 18:13:05 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,54 @@
+window.onkeyup = keyup;
+var inputTextValue;
+
+function clearInput() {
+  document.getElementById("searchTxt").value = "";
+}
+
+document.addEventListener("visibilitychange", (event) => {
+  if (document.visibilityState == "visible") {
+    return
+  } else {
+    clearInput();
+  }
+});
+
+function search(e) {
+  inputTextValue = e.target.value;
+  if (inputTextValue == "twitch") {
+    window.open("https://twitch.tv", "_blank");
+    clearInput();
+  } else if (inputTextValue == "youtube") {
+    window.open("https://www.youtube.com", "_blank");
+    clearInput();
+  } else if (inputTextValue == "osu") {
+    window.open("https://osu.ppy.sh/users/16780466", "_blank");
+    clearInput();
+  } else if (inputTextValue == "market") {
+    window.open("https://warframe.market", "_blank");
+    clearInput();
+  } else if (inputTextValue == "tiers") {
+    window.open("https://overframe.gg/tier-list/melee-weapons", "_blank");
+    clearInput();
+  } else if (inputTextValue == "xqc") {
+    window.open("https://twitch.tv/xqc", "_blank");
+    clearInput();
+  } else if (inputTextValue == "poke") {
+    window.open("https://twitch.tv/pokelawls", "_blank");
+    clearInput();
+  } else {
+    window.open("https://www.google.com/search?q=" + inputTextValue, "_blank");
+    clearInput();
+  }
+}
+
+function keyup(e) {
+  inputTextValue = e.target.value;
+  if (e.keyCode == 13) {
+    if (inputTextValue == "") {
+      return
+    } else {
+      search(e);
+    }
+  }
+}
